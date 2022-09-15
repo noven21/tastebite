32273ba5e9d222a0feca400785501b78543c4ba5
noven latigo <77792034+noven21@users.noreply.github.com>
2022-09-15 15:13:42 +0800

    getting random recipe and display it to the header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Header.css';
 
 const Header = () => {
-	return <div>Header</div>;
+	const [randomRecipe, setRandomRecipe] = useState([]);
+	const getRandomRecipe = async () => {
+		const res = await fetch(
+			`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_RECIPE_API_KEY}&number=1`
+		);
+
+		const data = await res.json();
+		console.log(data);
+		setRandomRecipe(data.recipes);
+	};
+
+	useEffect(() => {
+		getRandomRecipe();
+	}, []);
+
+	return (
+		<header>
+			{randomRecipe.map((recipe) => (
+				<div className='header ' key={recipe.id}>
+					<div className='header-img'>
+						<img src={recipe.image} alt={recipe.image} />
+					</div>
+					<div className='header-desc'>
+						<h1>{recipe.title}</h1>
+						<p
+							dangerouslySetInnerHTML={{
+								__html: recipe.summary.slice(0, 200),
+							}}
+						></p>
+					</div>
+				</div>
+			))}
+		</header>
+	);
 };
 
 export default Header;
